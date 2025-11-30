629da40c90299ef8fcce10a06d565e8ee9b266ce
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2025-11-30 18:51:11 +0300

    first commit

---
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -0,0 +1,34 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackDriver implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
+
+        caps.setCapability("browserstack.user", "floratds_7A1NL2");
+        caps.setCapability("browserstack.key", "sqqsJJGR7BeDwyArmhC3");
+        caps.setCapability("app", "bs://sample.app");
+        caps.setCapability("device", "Google Pixel 7 Pro");
+        caps.setCapability("os_version", "13.0");
+        caps.setCapability("project", "BrowserStack Sample");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
+
+        try {
+            return new RemoteWebDriver(
+                    new URL("https://hub.browserstack.com/wd/hub"), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
