669767372aee2e4a6485d88ec9f9dff4b2ba9762
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2026-01-11 01:06:10 +0300

    debug

--- src/test/java/drivers/DriverSelector.java
+++ src/test/java/drivers/DriverSelector.java
@@ -2,7 +2,7 @@ package drivers;
 
 public class DriverSelector {
     public static String getDriver() {
-        String deviceHost = System.getProperty("deviceHost");
+        String deviceHost = System.getProperty("deviceHost", "emulation");
         System.out.println("deviceHost: " + deviceHost);
 
         if ("browserstack".equals(deviceHost)) {
