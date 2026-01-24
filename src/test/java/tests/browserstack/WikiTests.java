efeec7de8ac761e7bd101a80444f5b3c1e161757
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2026-01-25 00:03:45 +0300

    add new app tests

--- src/test/java/tests/browserstack/WikiTests.java
+++ src/test/java/tests/browserstack/WikiTests.java
@@ -1,26 +1,43 @@
 package tests.browserstack;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.back;
+import static io.appium.java_client.AppiumBy.id;
 
 @Tag("browserstack")
 public class WikiTests extends TestBase {
-    String articleName = "Appium";
 
     @Test
-    @DisplayName("Article is founded from search")
-    void successfulSearchTest() {
-//        back();
-        mainScreen
-                .revealSearchInput()
-                .searchItem(articleName)
-                .checkContentFoundBS()
-                .checkItemFoundedBS(articleName);
+    @DisplayName("Редактор создания новой заметки открывается")
+    void successfulNewNoteTest() {
+        SelenideElement btnDeny = $(id("com.android.permissioncontroller:id/permission_deny_button"));
+        SelenideElement modal = $(id("com.android.permissioncontroller:id/grant_dialog"));
+        SelenideElement header = $(id("//android.view.View[@content-desc=\"ToDo\"]"));
+
+        modal.shouldBe(Condition.visible);
+        btnDeny.click();
+        header.shouldBe(Condition.visible);
     }
+
+//    String articleName = "Appium";
+//
+//    @Test
+//    @DisplayName("Article is founded from search")
+//    void successfulSearchTest() {
+////        back();
+//        mainScreen
+//                .revealSearchInput()
+//                .searchItem(articleName)
+//                .checkContentFoundBS()
+//                .checkItemFoundedBS(articleName);
+//    }
 
 //    @Test
 //    @DisplayName("Article opens from search results")
--- src/test/java/tests/emulation/WikiTests.java
+++ src/test/java/tests/emulation/WikiTests.java
@@ -1,42 +1,58 @@
 package tests.emulation;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import screens.ArticleScreen;
 import tests.TestBase;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.back;
+import static io.appium.java_client.AppiumBy.id;
 
 @Tag("emulation")
 public class WikiTests extends TestBase {
-    public ArticleScreen articleScreen = new ArticleScreen();
-    String articleName = "Appium";
+//    public ArticleScreen articleScreen = new ArticleScreen();
+//    String articleName = "Appium";
+
+//    @Test
+//    @DisplayName("Article is founded from search")
+//    void successfulSearchTest() {
+//        back();
+//        mainScreen
+//                .revealSearchInput()
+//                .searchItem(articleName)
+//                .checkContentFound()
+//                .checkItemFounded(articleName);
+//    }
+
+//    @Test
+//    @DisplayName("Article opens from search results")
+//    void successfulArticleOpenTest() {
+//        back();
+//        mainScreen
+//                .revealSearchInput()
+//                .searchItem(articleName)
+//                .checkContentFound()
+//                .clickFirstSearchItem();
+//
+//        articleScreen.closeModal() // todo написать метод
+//                // убрать увед, чтобы получить доступ к элементам на странице "got it"
+//                .checkArticleByName(articleName);
+//    }
 
     @Test
-    @DisplayName("Article is founded from search")
-    void successfulSearchTest() {
-        back();
-        mainScreen
-                .revealSearchInput()
-                .searchItem(articleName)
-                .checkContentFound()
-                .checkItemFounded(articleName);
-    }
+    @DisplayName("Редактор создания новой заметки открывается")
+    void successfulNewNoteTest() {
+        SelenideElement btnDeny = $(id("com.android.permissioncontroller:id/permission_deny_button"));
+        SelenideElement modal = $(id("com.android.permissioncontroller:id/grant_dialog"));
+        SelenideElement header = $(id("//android.view.View[@content-desc=\"ToDo\"]"));
 
-    @Test
-    @DisplayName("Article opens from search results")
-    void successfulArticleOpenTest() {
-        back();
-        mainScreen
-                .revealSearchInput()
-                .searchItem(articleName)
-                .checkContentFound()
-                .clickFirstSearchItem();
-
-        articleScreen.closeModal() // todo написать метод
-                // убрать увед, чтобы получить доступ к элементам на странице "got it"
-                .checkArticleByName(articleName);
+        modal.shouldBe(Condition.visible);
+        btnDeny.click();
+        header.shouldBe(Condition.visible);
     }
 
 
