eda946e596c82bf4002a9abcae80db4527d3793b
Iuliia Karnausova <iuliia.karnausova@enkod.io>
2026-01-24 22:19:30 +0300

    fix

--- src/test/java/screens/MainScreen.java
+++ src/test/java/screens/MainScreen.java
@@ -33,8 +33,8 @@ public class MainScreen {
         System.out.println("Search box displayed: " + searchField.isDisplayed());
         System.out.println("Search box enabled: " + searchField.isEnabled());
 
-        searchField.shouldBe(visible)
-                .click();
+//        searchField.shouldBe(visible)
+//                .click();
 
         searchContainer.shouldBe(visible).click();
 
